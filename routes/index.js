24877dc0b32260355a5d60f72465daa309e287ee
Alvaro <alvaroop97@gmail.com>
2018-05-03 17:19:32 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+const Sequelize = require('sequelize');
+const {models} = require('../models');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ALVARO' });
+});
+
+router.get('/credits', function(req, res, next) {
+    res.render('credits', { title: 'ÁLVARO' });
+});
+router.get('/quizzes', function(req, res, next) {
+    models.quiz.findAll()
+        .then(quizzes =>{
+        res.render('quizzes',{quizzes});
+})
+.catch(err =>{
+        res.locals.message = err.message;
+    res.status(404);
+    res.reedner('error');
+});
+});
+module.exports = router;
